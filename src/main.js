c3847806302d484da3a5d7fb22143c3d3f58b2e3
Tumbo <33678436+carltheghost@users.noreply.github.com>
2026-09-20 19:12:35 -0400

    feat(token): wire transfer console into main.js

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 import { createLivingRealityProjection } from './core/demo-projection.js?v=20260918-muse2';
+import './domains/token-transfers-ui.js?v=20260920-token-transfers1';
 import { resolveDefaultFeature } from './core/default-landing.js';
 import { createDeviceProjection, readBrowserProjectionPreferences } from './projections/device-projection.js';
 import { createDistributionExplorer } from './render/distribution-explorer.js';
