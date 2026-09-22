a64bc19533400dea792c072d4c8e52512663d23e
Tumbo <33678436+carltheghost@users.noreply.github.com>
2026-09-22 02:15:31 -0400

    chore: cache-bust polished TabEngine

--- src/render/tab-registry.js
+++ src/render/tab-registry.js
@@ -26,7 +26,7 @@
  * dock" and can never red-banner the boot.
  */
 
-import { TabEngine } from './tab-engine.js?v=20260922-rotating-cubes3';
+import { TabEngine } from './tab-engine.js?v=20260922-window-cubes4';
 import { initTabAR } from './tab-ar-tabs.js?v=20260922-cache2';
 
 const NARROW_QUERY = '(max-width:700px)';
